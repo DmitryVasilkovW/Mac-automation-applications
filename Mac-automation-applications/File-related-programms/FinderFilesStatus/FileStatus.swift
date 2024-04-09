847c15411a6a4cac0158df323f352f0cf1b94fb9
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-04-09 20:18:20 +0300

    feat: file status

--- Mac-automation-applications/File-related-programms/FinderFilesStatus/FileStatus.swift
+++ Mac-automation-applications/File-related-programms/FinderFilesStatus/FileStatus.swift
@@ -1,8 +1,39 @@
-//
-//  FileStatus.swift
-//  Mac-automation-applications
-//
-//  Created by Dmitry Vasilkov on 09.04.2024.
-//
-
 import Foundation
+
+func countFileTypesOnDesktop() 
+{
+    let fileManager = FileManager.default
+    var fileTypesCount = [String: Int]()
+
+    if let desktopDirectory = fileManager.urls(for: .desktopDirectory, in: .userDomainMask).first 
+    {
+        do 
+        {
+            let files = try fileManager.contentsOfDirectory(at: desktopDirectory, includingPropertiesForKeys: nil, options: [])
+
+            for file in files
+            {
+                let fileURL = desktopDirectory.appendingPathComponent(file.lastPathComponent)
+                let fileExtension = fileURL.pathExtension
+
+                if let count = fileTypesCount[fileExtension] 
+                {
+                    fileTypesCount[fileExtension] = count + 1
+                } 
+                else
+                {
+                    fileTypesCount[fileExtension] = 1
+                }
+            }
+
+            for (fileType, count) in fileTypesCount 
+            {
+                print("Тип файла: \(fileType), Количество: \(count)")
+            }
+        }
+        catch
+        {
+            print("Ошибка при чтении файлов с рабочего стола: \(error)")
+        }
+    }
+}
